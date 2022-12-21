183621501566f95e814cfa8376a822aab0ac2981
adam urbanowicz <adam.urbanow@gmail.com>
2022-12-21 13:52:51 +0100

    lab 11

---
+++ westeros/westeros-diagnostics-tools/src/main/java/com/westeros/diagnostics/runners/DiagnosticsRunner.java
@@ -0,0 +1,12 @@
+package com.westeros.diagnostics.runners;
+
+import com.westeros.diagnostics.service.contract.Diagnostics;
+
+import java.util.List;
+
+public class DiagnosticsRunner implements IRunDiagnoses {
+    @Override
+    public List<Diagnostics> runAll() {
+        return null;
+    }
+}
