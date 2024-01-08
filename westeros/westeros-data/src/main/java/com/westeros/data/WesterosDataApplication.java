9e06e2db1f917b3ea597d3bf0974d5a9f6d0531f
adam urbanowicz <adam.urbanow@gmail.com>
2024-01-08 17:01:02 +0100

    2023

--- westeros/westeros-data/src/main/java/com/westeros/data/WesterosDataApplication.java
+++ westeros/westeros-data/src/main/java/com/westeros/data/WesterosDataApplication.java
@@ -11,29 +11,11 @@ import java.util.List;
 @SpringBootApplication
 public class WesterosDataApplication implements CommandLineRunner {
 
-    private final ICatalogData db;
-
-    public WesterosDataApplication(ICatalogData dataCatalog) {
-        this.db = dataCatalog;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(WesterosDataApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-
-        var cmp = db.getCompanies();
-
-        var company = new Company();
-//        var movie = new Movie();
-//        dataCatalog.getCompanies().save(company);
-//        dataCatalog.getMovies().save(movie);
-//        company.getMovies().add(movie);
-//        movie.getProductionCompanies().add(company);
-//        dataCatalog.getCompanies().save(company);
-
-
     }
 }
